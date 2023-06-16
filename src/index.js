ba66d79ba1ab3bc46197353dceb3ec7dec28812d
Sara Bonyadian <sara.bonyadian@gmail.com>
2023-06-16 14:53:05 +0330

    Add Javascript codes

---
+++ src/index.js
@@ -0,0 +1,173 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80,
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50,
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20,
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100,
+  },
+  oslo: {
+    temp: -5,
+    humidity: 20,
+  },
+};
+
+// write your code here
+// console.log(weather["paris"].temp);
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let apiKey = "5863935ee9cca4c02ed68203f807c65b";
+let units = "metric";
+// search.addEventListener("click", searchCity);
+
+// function searchCity() {
+//   let name = prompt("Enter a city");
+
+//   if (name in weather) {
+//     alert(
+//       `It is currently ${weather[name].temp}°C  in ${name} with a humidity of ${weather[name].humidity}%`
+//     );
+//   } else {
+//     alert(
+//       `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${name}`
+//     );
+//   }
+// }
+const searchBtn = document.getElementById("search");
+const temp = document.getElementById("temperature");
+let tempwithCelesium = temp.innerHTML;
+const changeToFarenheit = document.getElementById("fahrenheit");
+const changeToCelsius = document.getElementById("celsius");
+const cityName = document.getElementById("city-name");
+const currentBtn = document.getElementById("current");
+const humiditity = document.getElementById("humiditity");
+const wind = document.getElementById("wind");
+const description = document.getElementById("description");
+
+function searchCity(cityName, cities) {
+  if (cityName in cities) {
+  } else {
+    alert(
+      `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${name}`
+    );
+  }
+}
+function displayInfoCity(event) {
+  event.preventDefault();
+  const cityInput = document.getElementById("cityInput");
+  const cityInputVlue = cityInput.value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputVlue}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTempAndNameCity);
+  // const cityName = document.getElementById("city-name");
+  // cityName.innerHTML = cityInputVlue;
+
+  // if (cityInputVlue in weather) {
+  //   temp.innerHTML = `${weather[cityInputVlue].temp}°C`;
+  //   tempwithCelesium = temp.innerHTML;
+  // } else {
+  //   alert(
+  //     `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${name}`
+  //   );
+  // }
+}
+
+function displayCurrentTemp(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showCurrentTemp);
+}
+
+function showCurrentTemp(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTempAndNameCity);
+}
+
+function showTempAndNameCity(response) {
+  let infoCity = response.data;
+  let getCityName = infoCity.name;
+  let getTemp = Math.round(infoCity.main.temp);
+  let getHumiditity = infoCity.main.humidity;
+  let getWind = infoCity.wind.speed;
+  let getDescription = infoCity.weather[0].description;
+  console.log(infoCity);
+  cityName.innerHTML = getCityName;
+  temp.innerHTML = `${getTemp}°C`;
+  tempwithCelesium = temp.innerHTML;
+  humiditity.innerHTML = getHumiditity;
+  wind.innerHTML = getWind;
+  description.innerHTML = getDescription;
+}
+
+function formatTime(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+function showTime() {
+  const date = new Date();
+  const day = date.getDay();
+  const dayName = dayNames[day];
+  const dateInfo = document.getElementById("date-info");
+  const timeInfo = document.getElementById("time");
+
+  dateInfo.innerHTML = dayName;
+  timeInfo.innerHTML = formatTime(date);
+}
+
+let isCelesium = true;
+function converteToCelesium(event) {
+  event.preventDefault();
+  isCelesium = true;
+  displayTemp();
+}
+
+function converteToFarenheit(event) {
+  event.preventDefault();
+  isCelesium = false;
+  displayTemp();
+}
+
+function displayTemp() {
+  if (isCelesium) {
+    temp.innerHTML = tempwithCelesium;
+  } else {
+    const tempWithFarenhiet = (parseInt(temp.innerHTML) * 9) / 5 + 32 + "°F";
+    temp.innerHTML = tempWithFarenhiet;
+  }
+}
+
+showTime();
+
+searchBtn.addEventListener("click", displayInfoCity);
+
+currentBtn.addEventListener("click", displayCurrentTemp);
+
+changeToFarenheit.addEventListener("click", converteToFarenheit);
+
+changeToCelsius.addEventListener("click", converteToCelesium);
